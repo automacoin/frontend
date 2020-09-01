60bb66ddbcedf54431ab231ce326ef072727998e
Alessio Proietti <alessio.proietti@protonmail.com>
2020-09-01 21:15:15 +0200

    Added Pageloader and Switch Button.

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,8 @@
+/* Copyright (c) 2020 AutomaCoin*/
+
+/* Eventually this mocked delay will be deleted. It's a showcase for the PageLoader*/
+window.addEventListener('load', function () {
+    setTimeout(() => document.querySelector('#pageloader').classList.remove("is-active"), 2500);
+});
+
 console.log('Hello from Automacoin Client!');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* Copyright (c) 2020 AutomaCoin*/
+
 import './styles.scss';
 import 'alpinejs';
 import './app/app';
