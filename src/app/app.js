e6e1c571239335ff8b6f369f9e22a4018e8a1158
Alessio Proietti <alessio.proietti@protonmail.com>
2020-09-02 13:23:17 +0200

    Ok so Jest has been reintroduced. Turned out error was a feature of ecmascript: http://www.ecma-international.org/ ecma-262/6.0/#sec-abstract-module-records
    good.

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,5 @@
 /* Copyright (c) 2020 AutomaCoin*/
+
+export function ultimateQuestion() {
+    return 42;
+}
---
+++ test/suite.test.js
@@ -0,0 +1,5 @@
+import { ultimateQuestion } from '../src/app/app'
+
+it ('dummy test', () => {
+    expect(ultimateQuestion()).toBe(42);
+})
