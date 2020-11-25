ff82b587e5ad339dd8b4079c7f5edc9abc37a4a2
Alessio Proietti <alessio.proietti@protonmail.com>
2020-11-25 12:31:36 +0100

    Main routine implemented.

--- src/app/app.js
+++ src/app/app.js
@@ -104,7 +104,7 @@ export function userProfileComponent() {
                 if (isConnect) {
                     console.log(isConnect);
 
-                    const response = await harvester.account(window.zilPay.wallet.defaultAccount.base16, Spruce.store('wallet').nonce, (await window.zilPay.wallet.sign(MESSAGE.SIGNIN + Spruce.store('wallet').nonce)).signature);
+                    const response = await harvester.account(window.zilPay.wallet.defaultAccount.base16, Spruce.store('wallet').nonce, await window.zilPay.wallet.sign('I\'m in')/*MESSAGE.SIGNIN + Spruce.store('wallet').nonce)).signature*/);
                     if (response.client) {
 
                         this.logged = true;
@@ -138,6 +138,9 @@ export function userProfileComponent() {
 export function optionsComponent() {
 
     return {
+
+        aborter: null,
+
         spinner: null,
 
         tab: 'console',
@@ -148,26 +151,69 @@ export function optionsComponent() {
 
         workunit: null,
 
+        control: 'Idle.',
+
         init: function () {
             this.spinner = new Spinner(SPINNEROPTS)
         },
 
-        abort: function () {
+        brake: function () {
+            PubSub.publish('ERROR', 'Execution stopped by User.');
             Spruce.store('engine').state = 'ready';
             this.spinner.stop();
         },
 
-        fire: async function () {
-            try {
+        go: async function () {
+
+            const control = document.getElementById('engineControl');
+            if (control.checked == true) {
+
                 const target = document.getElementById('spinner');
                 this.spinner.spin(target);
+
+                this.control = "Up and running";
+                console.log("run")
+                //this.routine();
+
+                while (document.getElementById('engineControl').checked === true) {
+                    await this.fetch();
+                    await this.fire();
+                }
+
+                PubSub.publish('PROBLEMS', 'Execution stopped by User.');
+                Spruce.store('engine').state = 'ready';
+                this.spinner.stop();
+
+            } else {
+                this.control = "Idle";
+                console.log("idle")
+            }
+
+        },
+
+        /* it would probabably hit max call stack.
+        routine: async function () {
+             if (document.getElementById('engineControl').checked === true) {
+                 await this.fetch();
+                 await this.fire();
+                 this.routine();
+             } else {
+                 PubSub.publish('PROBLEMS', 'Execution stopped by User.');
+                 Spruce.store('engine').state = 'ready';
+                 this.spinner.stop();
+                 return
+             }
+         },*/
+
+        fire: async function () {
+            try {
                 Spruce.store('engine').state = 'ongoing';
                 PubSub.publish('TERMINAL', 'Starting computation...');
 
                 toast({
                     message: "Engine Started!",
                     type: "is-danger",
-                    duration: 1250,
+                    duration: 500,
                     dismissible: true,
                     animate: { in: "fadeIn", out: "fadeOut" }
                 });
@@ -193,28 +239,25 @@ export function optionsComponent() {
             } finally {
                 Spruce.store('engine').state = 'ready';
                 this.loading = false;
-                this.spinner.stop();
             }
         },
 
         fetch: async function () {
 
             try {
-                const target = document.getElementById('spinner');
-                this.spinner.spin(target);
 
                 Spruce.store('engine').state = 'fetching';
 
                 PubSub.publish('TERMINAL', 'Fetching Turing Machines ...');
 
-                const signature = await window.zilPay.wallet.sign('hello');
+                const signature = '';
 
                 this.workunit = await harvester.allocate(Spruce.store('wallet').account, Spruce.store('wallet').nonce, signature);
 
                 toast({
                     message: "Fetching new Data...",
                     type: "is-warning",
-                    duration: 1250,
+                    duration: 500,
                     dismissible: true,
                     animate: { in: "fadeIn", out: "fadeOut" }
                 });
@@ -225,7 +268,6 @@ export function optionsComponent() {
             } finally {
 
                 Spruce.store('engine').state = 'ready';
-                this.spinner.stop();
                 this.loading = false;
             }
 
